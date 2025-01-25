6b7600e98770f17d7f54ed8ee49ec1cbcef6ec57
lhohan <2922363+lhohan@users.noreply.github.com>
2025-01-25 20:34:03 +0100

    test: Remove duplicate test

--- tests/cli.rs
+++ tests/cli.rs
@@ -40,41 +40,6 @@ fn test_basic_time_tracking() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
-fn test_basic_time_tracking_old() -> Result<(), Box<dyn std::error::Error>> {
-    // Create a temporary test directory
-    let temp = assert_fs::TempDir::new()?;
-
-    // Create input file with test content
-    let input_file = temp.child("day1.md");
-    input_file.write_str(
-        r#"## TT 2025-01-15
-
-- #sport 30m
-- #coding 2p
-- #journaling 20m
-- #sport 1h"#,
-    )?;
-
-    let mut c = Command::cargo_bin("tt")?;
-
-    c.arg("--input")
-        .arg(input_file.path())
-        .assert()
-        .success()
-        .stdout(predicate::str::contains(
-            "journaling............ 0h 20m ( 12%)",
-        ))
-        .stdout(predicate::str::contains(
-            "coding................ 1h  0m ( 35%)",
-        ))
-        .stdout(predicate::str::contains(
-            "sport................. 1h 30m ( 53%)",
-        ));
-
-    Ok(())
-}
-
-#[test]
 fn test_verbose_output() -> Result<(), Box<dyn std::error::Error>> {
     CommandSpec::new()
         .with_verbose()
