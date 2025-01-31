a4761899ce7aa8888849763ab4f449ca3618b016
lhohan <2922363+lhohan@users.noreply.github.com>
2025-01-31 10:29:15 +0100

    refactor: split into smaller functions

--- src/lib.rs
+++ src/lib.rs
@@ -24,39 +24,14 @@ pub fn run(
         )
     })?;
     // note: something is not quite right with project_detail_request: it serves 2 purposes which may be clearly encoded: selecting the tasks overview for a project + filtering entries
-    let filter = project_detail_request
-        .map(String::from)
-        .clone()
-        .map(Filter::Project)
-        .into_iter()
-        .chain(from_date.map(|date| Filter::DateRange(DateRange::new_from_date(date))))
-        .reduce(|acc, filter| Filter::And(Box::new(acc), Box::new(filter)));
+    let filter = create_filter(project_detail_request, from_date);
 
-    let report = parsing::get_entries(&content, &filter).map(|parse_result| {
-        let entries = parse_result.entries().clone(); // todo: fix clone, make ParseResult return reference or immutable structure?
-        let report = if let Some(project) = project_detail_request.map(String::from) {
-            Report::new_project_detail(
-                entries,
-                project.to_string(),
-                parse_result.start_date(),
-                parse_result.end_date(),
-                parse_result.days(),
-            )
-        } else {
-            Report::new_overview(
-                entries,
-                parse_result.start_date(),
-                parse_result.end_date(),
-                parse_result.days(),
-            )
-        };
-        (report, parse_result.errors().clone())
-    });
+    let project_detail_request = project_detail_request.map(String::from);
+    let report_result = create_report(&content, &filter, project_detail_request);
 
-    match report {
+    match report_result {
         Some((report, errors)) => {
             println!("{}", report);
-
             errors
                 .into_iter()
                 .for_each(|error| println!("Warning: {}", error));
@@ -65,4 +40,43 @@ pub fn run(
     }
 
     Ok(())
+}
+
+fn create_filter(
+    project_detail_request: Option<&str>,
+    from_date: Option<StartDate>,
+) -> Option<Filter> {
+    let project_filter = project_detail_request.map(|project| Filter::Project(project.to_string()));
+    let date_filter = from_date.map(|date| Filter::DateRange(DateRange::new_from_date(date)));
+
+    project_filter
+        .into_iter()
+        .chain(date_filter)
+        .reduce(|acc, filter| Filter::And(Box::new(acc), Box::new(filter)))
+}
+
+fn create_report(
+    content: &str,
+    filter: &Option<Filter>,
+    project_detail_request: Option<String>,
+) -> Option<(Report, Vec<ParseError>)> {
+    parsing::get_entries(content, filter).map(|parse_result| {
+        let entries = parse_result.entries().clone();
+        let report = match project_detail_request {
+            Some(project) => Report::new_project_detail(
+                entries,
+                project.clone(),
+                parse_result.start_date(),
+                parse_result.end_date(),
+                parse_result.days(),
+            ),
+            _ => Report::new_overview(
+                entries,
+                parse_result.start_date(),
+                parse_result.end_date(),
+                parse_result.days(),
+            ),
+        };
+        (report, parse_result.errors().clone())
+    })
 }
