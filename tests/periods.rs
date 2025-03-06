215f0490ba0bf2b088badae4edf96deadb98e5c9
lhohan <2922363+lhohan@users.noreply.github.com>
2025-03-06 10:58:04 +0100

    test: Expand period today tests

--- tests/periods.rs
+++ tests/periods.rs
@@ -87,22 +87,35 @@ fn this_month_report(#[values("this-month", "tm")] this_month_value: &str) {
 }
 
 #[rstest]
-fn today_report(#[values("today", "t")] value: &str) {
+fn today_report(
+    #[values("today", "t")] value: &str,
+    #[values(
+        ("2020-01-01", "1h 00m"),
+        ("2020-01-02", "2h 00m"),
+        ("2020-01-03", "4h 00m"),
+    )]
+    at_date_and_expected_duration: (&str, &str),
+) {
     let content = r"## TT 2020-01-01
     - #dev 1h Task1
     ## TT 2020-01-02
     - #dev 2h Task2
-    ## TT 2020-02-03
+    ## TT 2020-01-03
     - #dev 4h Task3";
+
+    let at_date = at_date_and_expected_duration.0;
+
+    let expected_output = format!("today ({})", at_date);
+    let expected_duration = at_date_and_expected_duration.1;
 
     CommandSpec::new()
         .with_file(content)
-        .at_date("2020-01-02")
+        .at_date(at_date)
         .with_period(value)
         .when_run()
         .should_succeed()
-        .expect_output("today (2020-01-02)")
+        .expect_output(&expected_output)
         .expect_project("dev")
-        .taking("2h 00m") // Only tasks 'today'
+        .taking(expected_duration) // Only tasks of 'today'
         .validate();
 }
